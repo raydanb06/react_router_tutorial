c56dfa9672e0360f8735fbabaf02c2b53dadfdef
Raydan Bala <raydanb@gmail.com>
2023-05-08 23:48:22 -0700

    finished the index route for the <Outlet>

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+export default function Index() {
+  return <>
+    <p id="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check out {" "}
+      <a href="https://reactrouter.com">
+        the docs at reactrouter.com
+      </a>
+      .
+    </p>
+    <p>
+    Voila! No more blank space. It&apos;s common to put dashboards, stats, feeds, etc. at index routes. They can participate in data loading as well.
+    </p>
+  </>
+}
